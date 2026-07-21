d41860f95853aead248a200ecb2923d26d3625a6
mina <minashabani101@gmail.com>
2026-07-21 03:00:13 -0400

    add exstart state bug feat(exstart): add ExStart state bug detection logic

--- core/ospf_state.py
+++ core/ospf_state.py
@@ -10,11 +10,14 @@ class OspfValidationState:
     netmask: Optional[str] = None
     hello_interval: Optional[int] = None
     dead_interval: Optional[int] = None
+    mtu: Optional[int] =None
+    
 
     def update_from_json(self, data: Dict[str, Any]):
         self.options = data.get("options", self.options)
         self.netmask = data.get("netmask", self.netmask)
         self.hello_interval = data.get("hello_interval", self.hello_interval)
         self.dead_interval = data.get("dead_interval", self.dead_interval)
+        self.mtu = data.get("mtu",self.mtu)
 
 global_ospf_state = OspfValidationState()
--- registry.py
+++ registry.py
@@ -1,10 +1,11 @@
 from typing import Dict, Any , Optional
 from base import OspfPacketHandler
 from ospf_handlers.hello_2way import *
+from ospf_handlers.exstart import *
 
 _REGISTRY: Dict[int, OspfPacketHandler] = {
-    2: hello_2wayHandler()  
-    # 4: OspfLsuHandler(),    # نوع ۴ مربوط به بسته‌های LSU
+    2: hello_2wayHandler() , 
+    3: dbd_exstartHandler()
 }
 
 def get_handler(packet_type: int) -> Optional[OspfPacketHandler]:
